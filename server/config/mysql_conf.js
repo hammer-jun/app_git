bccb9f141f65639196b79f27afc480b461fdc80e
hammer-jun <hammer-jun>
2021-01-05 14:34:26 +0800

    git  add .

---
+++ server/config/mysql_conf.js
@@ -0,0 +1,14 @@
+"use strict"
+module.exports = {
+    host: "192.144.239.176",
+    port: 3306,
+    user: "root",
+    password: "beihua426",
+    database: "db2",
+    connectionLimit: 100,
+    queueLimit: 0,
+    acquireTimeout: 10000,
+    waitForConnections: true,
+    timezone: "+08:00",
+    dateStrings: true
+}
---
+++ server/config/session_conf.js
@@ -0,0 +1,9 @@
+"use strict"
+module.exports = {
+    secret: 'secret',
+    resave: 'true',
+    saveUninitialized: true,
+    cookie:{
+        maxAge:1000*60*60*24*14
+    }
+}
